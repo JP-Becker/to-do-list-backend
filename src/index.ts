6523475940f4c5e037e36b930f51b934e19468cc
JP-Becker <jpbecker20@gmail.com>
2023-01-26 10:27:08 -0300

    create user functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
+import { TUserDB, TTasksDB, TUserTaskDB, TTasksWithUsersDB } from './types'
 
 const app = express()
 
@@ -32,7 +33,7 @@ app.get("/ping", async (req: Request, res: Response) => {
 
 
 // get all users
-app.get('/users', async (req, res) => {
+app.get('/users', async (req: Request, res: Response) => {
     try {
         const result = await db("users")
         res.status(200).send({ 'Lista de usuários cadastrados': result})
@@ -53,7 +54,7 @@ app.get('/users', async (req, res) => {
 
 
 // search user by name via query params
-app.get('/users/search', async (req, res) => {
+app.get('/users/search', async (req: Request, res: Response) => {
     try {
         const q = req.query.q as string
 
@@ -82,7 +83,7 @@ app.get('/users/search', async (req, res) => {
 
 
 // search user by ID via path params
-app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', async (req: Request, res: Response) => {
     try {
         const searchedId = req.params.id
 
@@ -94,6 +95,79 @@ app.get('/users/:id', async (req, res) => {
             res.status(404)
             throw new Error("A 'id' buscada não existe no banco de dados");
         }
+
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+
+// create a new user
+app.post('/users', async (req: Request, res: Response) => {
+    try {
+        const { id, name, email, password } = req.body
+
+        if (typeof id !== "string") {
+            res.status(400)
+            throw new Error("Tipo de 'id' inválido");
+        }
+        if (id.length < 4) {
+            res.status(400)
+            throw new Error("'id' deve possuir pelo menos 4 caracteres")
+        }
+        if (typeof name !== "string") {
+            res.status(400)
+            throw new Error("Tipo de 'name' inválido");
+        }
+        if (name.length < 4) {
+            res.status(400)
+            throw new Error("'name' deve possuir pelo menos 4 caracteres");
+        }
+        if (typeof email !== "string") {
+            res.status(400)
+            throw new Error("Tipo de 'email' inválido");
+        }
+        if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g)) {
+			throw new Error("'password' deve possuir entre 8 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial")
+		}
+
+        const [userIdAlreadyExists] : TUserDB[] | undefined[] = await db("users").where({id})
+
+        if (userIdAlreadyExists) {
+            res.status(400)
+            throw new Error("Essa 'id' já existe");
+        }
+
+        const [userEmailAlreadyExists] : TUserDB[] | undefined[] = await db("users").where({email})
+
+        if (userEmailAlreadyExists) {
+            res.status(400)
+            throw new Error("Esse 'email' já existe");
+        }
+        
+        const newUser: TUserDB = {
+            id,
+            name,
+            email,
+            password
+        }
+        
+        await db("users").insert(newUser)
+
+        res.status(201).send({
+            message: "User criado com sucesso",
+            user: newUser
+        })
 
     } catch (error) {
         console.log(error)
